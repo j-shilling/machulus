940eadfafddb01483e971811ee21089b68648a2a
Jake Shilling <shilling.jake@gmail.com>
2018-05-25 08:33:51 -0400

    adding kernel panic and multiboot functionality

--- kernel/arch/i386/arch-init.c
+++ kernel/arch/i386/arch-init.c
@@ -1,7 +1,15 @@
+#include <kernel/panic.h>
+#include <stddef.h>
+
+#include "multiboot.h"
+
 extern void tty_init (void);
 
 void
-arch_init(void)
+arch_init(multiboot_info_t *mbi)
 {
   tty_init();
+
+  if (NULL == mbi)
+    panic ("We were not loaded by a multiboot compliant bootloader.");
 }
---
+++ kernel/include/kernel/panic.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+void panic (const char *, ...);
+
+__END_DECLS
