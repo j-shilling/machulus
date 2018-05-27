36c551e30994e5ac9221d1ae1d529b016130603e
Jake Shilling <shilling.jake@gmail.com>
2018-05-27 12:52:10 -0400

    Trying to initialize frame manager in assembly before entering the higher half kernel

--- kernel/arch/i386/arch-init.c
+++ kernel/arch/i386/arch-init.c
@@ -3,9 +3,10 @@
 #include <stdint.h>
 
 #include "multiboot.h"
+#include "frames.h"
 
 extern void tty_init (void);
-extern void paging_init (uint32_t mmap_length, uint32_t mmap_addr);
+//extern void paging_init (void);
 
 void
 arch_init(multiboot_memory_map_t  *mmap, uint32_t mmap_length)
@@ -15,4 +16,6 @@ arch_init(multiboot_memory_map_t  *mmap, uint32_t mmap_length)
   if (NULL == mmap)
     panic ("We did not get a memory map from the bootloader.");
 
+ // frames_init ((uint32_t)mmap, mmap_length);
+  //paging_init ();
 }
--- kernel/arch/i386/paging.h
+++ kernel/arch/i386/paging.h
@@ -1,16 +1,28 @@
 #pragma once
 
 #define KERNEL_VBASE		(0xC0000000)
+#define PHYS_ADDR(x)		((x) - KERNEL_VBASE)
+
+#define FRAME_STACK_VBASE	(KERNEL_VBASE + 0x10000000)
+#define KSTACK_VBASE		(KERNEL_VBASE + 0x20000000)
+
 #define PAGE_SIZE		(0x1000)
 
 #define PAGE_FLAG_READ_WRITE	(1 << 1) /* Page is read-write accessible */
 #define PAGE_FLAG_PRESENT	(1 << 0) /* Page is present */
 
+#define KPAGE_FLAGS		(PAGE_FLAG_PRESENT | PAGE_FLAG_READ_WRITE)
+
 #ifndef ASM_FILE
 
+#include <sys/cdefs.h>
 #include <stdint.h>
 
-extern uint32_t kernel_page_directory[1024];
-extern uint32_t kernel_page_table[1024];
+__BEGIN_DECLS
+
+int paging_map_frame_stack (uint32_t, uint32_t);
+int paging_unmap_frame_stack (uint32_t, uint32_t *);
+
+__END_DECLS
 
 #endif /* !ASM_FILE */
