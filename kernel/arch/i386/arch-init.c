94559bf4c903e765ed5a711a11b8bc4dbda40ce5
Jake Shilling <shilling.jake@gmail.com>
2018-06-08 17:02:02 -0400

    cleaning up

--- kernel/arch/i386/arch-init.c
+++ kernel/arch/i386/arch-init.c
@@ -1,16 +1,12 @@
-#include <kernel/panic.h>
-#include <stddef.h>
 #include <stdint.h>
 
 #include "frames.h"
 
 extern void tty_init (void);
-//extern void paging_init (void);
 
 void
 arch_init(uint32_t mmap_addr, uint32_t mmap_length)
 {
   tty_init();
   frames_init (mmap_addr, mmap_length);
-  //paging_init ();
 }
