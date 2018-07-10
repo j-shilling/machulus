f1a8a9c6ab7193a5aa8e5840c4edbeb6284c97d5
Jake Shilling <shilling.jake@gmail.com>
2018-07-10 06:58:23 -0400

    Generating linker script with CPP

---
+++ kernel/include/config.h
@@ -0,0 +1,41 @@
+/* ./include/config.h.  Generated from config.h.in by configure.  */
+/* ./include/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the `gcc' library (-lgcc). */
+#define HAVE_LIBGCC 1
+
+/* Name of package */
+#define PACKAGE "machulus"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "machulus"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "machulus 0.0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "machulus"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.0.1"
+
+/* Version number of package */
+#define VERSION "0.0.1"
+
+/* x86 version */
+#define __CPU_VERSION__ 686
+
+/* debug mode */
+#define __DEBUG__ 0
+
+/* use physical address extension */
+#define __PAE__ 0
+
+/* 64-bit kernel */
+#define __x86_64__ 0
