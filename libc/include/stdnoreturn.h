ba289bfec0ebb5e514376170f30e67ca93ddac1e
j-shilling <shilling.jake@gmail.com>
2018-09-04 07:17:25 -0400

    Removing left over files in top level directory

--- libc/include/stdnoreturn.h
+++
@@ -1,9 +0,0 @@
-#include <sys/cdefs.h>
-
-#if __STD_C11
-#  define noreturn		_Noreturn
-#elif __GNUC_PREREQ (2,8)
-#  define noreturn		__attribute__ ((__noreturn__))
-#else
-#  define noreturn		/* ignore */
-#endif
