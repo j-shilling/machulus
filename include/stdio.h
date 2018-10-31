ce57ab24ba8df24d6e5226525e85f06a5322fa2b
j-shilling <shilling.jake@gmail.com>
2018-10-31 08:39:02 -0400

    Updating copyright

--- include/stdio.h
+++ include/stdio.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 jshilling
+ * Copyright (C) 2018 Jake Shilling
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
