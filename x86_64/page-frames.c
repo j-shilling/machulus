888e1e14794b6d1bfc2cf870f1eed4105b0e0173
Jake Shilling <shilling.jake@gmail.com>
2018-11-23 23:02:04 -0500

    Finding number of available frames

--- x86_64/page-frames.c
+++ x86_64/page-frames.c
@@ -26,6 +26,7 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
 
 int
 init_page_frames (void *multiboot_addr)
@@ -50,6 +51,43 @@ init_page_frames (void *multiboot_addr)
   /* Check whether we found the memory map */
   if (NULL == mmap)
     return ENOMMAP;
+
+  /* Next we need to know the total number of frames on
+     the system. */
+  size_t bytes;
+  struct multiboot_mmap_entry *entry = mmap->entries;
+  while ((char *)entry < ((char *)mmap + mmap->size))
+    {
+      /* Get last address of this entry */
+      bytes += entry->len;
+      entry = (struct multiboot_mmap_entry *)
+	((char *)entry + mmap->entry_size);
+    }
+
+  size_t frames0 = bytes / 0x1000;
+  size_t frames1 = frames0 / 2;
+  size_t frames2 = frames1 / 2;
+  size_t frames3 = frames2 / 2;
+  size_t frames4 = frames3 / 2;
+  size_t frames5 = frames4 / 2;
+  size_t frames6 = frames5 / 2;
+  size_t frames7 = frames6 / 2;
+  size_t frames8 = frames7 / 2;
+  size_t frames9 = frames8 / 2;
+  size_t frames10 = frames9 / 2;
+
+  printf ("Number of page frames:\n\n");
+  printf ("0: %10zu\n", frames0);
+  printf ("1: %10zu\n", frames1);
+  printf ("2: %10zu\n", frames2);
+  printf ("3: %10zu\n", frames3);
+  printf ("4: %10zu\n", frames4);
+  printf ("5: %10zu\n", frames5);
+  printf ("6: %10zu\n", frames6);
+  printf ("7: %10zu\n", frames7);
+  printf ("8: %10zu\n", frames8);
+  printf ("9: %10zu\n", frames9);
+  printf ("10: %9zu\n", frames10);
   
   return 0;
 }
