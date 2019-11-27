ea47f722bdb390da7a5f746d8c1ef1e5968d2ba9
Jake Shilling <shilling.jake@gmail.com>
2019-11-27 13:59:50 -0500

    adding initial page tables

--- inc/memory.h
+++ inc/memory.h
@@ -2,3 +2,8 @@
 
 #define KERNEL_OFFSET 0xffffffff80000000
 #define PAGE_SIZE 0x1000
+
+#define PAGE_PRESENT (1 << 0)
+#define PAGE_WRITABLE (1 << 1)
+
+#define virtual_to_physical(addr) (addr - KERNEL_OFFSET)
