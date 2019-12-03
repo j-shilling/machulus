f40be9c98e4844d2c28c9fe3272fa2b9c78bc1db
Jake Shilling <shilling.jake@gmail.com>
2019-12-02 20:06:28 -0500

    Adding doxygen comments

--- inc/file.h
+++ inc/file.h
@@ -1,11 +1,33 @@
+/**
+ * @file file.h
+ * @brief Implements FILE for use in kernel space.
+ */
+
 #pragma once
 
 #include <stdio.h>
 #include <stdbool.h>
 
+/**
+ * @brief Implementation of FILE.
+ *
+ * @details The C standard library uses an opaque FILE object to represent an
+ * I/O stream. This structure provides a stream implementation to work
+ * with the I/O functions which run in kernel-space.
+ *
+ * @see stdio.h
+ */
 struct FILE {
-  size_t (*fwrite)(const void *, size_t, size_t, FILE *);
+  /**
+   * @brief References an implementation of fwrite.
+   *
+   * @see fwrite
+   */
+  size_t (*fwrite)(const void *ptr, size_t size, size_t nmemb, FILE *stream);
 
+  /**
+   * @brief A spinlock to provide synchronized access to this stream.
+   */
   volatile bool lock;
 };
 
