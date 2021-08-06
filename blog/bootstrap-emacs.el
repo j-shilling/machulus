86a870915096a2fcc4f91c591382193c5a6fc95d
Jake Shilling <shilling.jake@gmail.com>
2021-08-05 22:36:15 -0400

    Create pipeline template to export blog site

---
+++ blog/bootstrap-emacs.el
@@ -0,0 +1,7 @@
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-refresh-contents)
+(package-install 'org-plus-contrib)
+(package-install 'htmlize)
---
+++ blog/publish.el
@@ -0,0 +1,61 @@
+;; publish.el --- Publish org-mode project on Gitlab Pages
+;; Original Author: Rasmus
+
+;;; Commentary:
+;;;
+;;; Taken from Rasmus and edited for my purposes. I downloaded the original
+;;; from: https://gitlab.com/pages/org-mode/-/raw/master/publish.el
+
+;;; Code:
+
+(require 'org)
+(require 'ox-publish)
+
+;; setting to nil, avoids "Author: x" at the bottom
+(setq user-full-name nil)
+
+(setq org-export-with-section-numbers nil
+      org-export-with-smart-quotes t
+      org-export-with-toc nil)
+
+(setq org-html-divs '((preamble "header" "top")
+                      (content "main" "content")
+                      (postamble "footer" "postamble"))
+      org-html-container-element "section"
+      org-html-metadata-timestamp-format "%Y-%m-%d"
+      org-html-checkbox-type 'html
+      org-html-html5-fancy t
+      org-html-validation-link nil
+      org-html-doctype "html5")
+
+(defvar site-attachments
+  (regexp-opt '("jpg" "jpeg" "gif" "png" "svg"
+                "ico" "cur" "css" "js" "woff" "html" "pdf"))
+  "File types that are published as static files.")
+
+(setq org-publish-project-alist
+      (list
+       (list "site-org"
+             :base-directory "./blog"
+             :base-extension "org"
+             :recursive t
+             :publishing-function '(org-html-publish-to-html)
+             :publishing-directory "./public"
+             :exclude (regexp-opt '("README" "draft"))
+             :auto-sitemap t
+             :sitemap-filename "index.org"
+             :sitemap-file-entry-format "%d *%t*"
+             :html-head-extra "<link rel=\"icon\" type=\"image/x-icon\" href=\"/favicon.ico\"/>"
+             :sitemap-style 'list
+             :sitemap-sort-files 'anti-chronologically)
+       (list "site-static"
+             :base-directory "."
+             :exclude "public/"
+             :base-extension site-attachments
+             :publishing-directory "./public"
+             :publishing-function 'org-publish-attachment
+             :recursive t)
+       (list "site" :components '("site-org"))))
+
+(provide 'publish)
+;;; publish.el ends here
