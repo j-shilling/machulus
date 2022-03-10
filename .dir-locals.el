72da985aae5822d4c8d3782918b0a6634b436cb2
Jake Shilling <shilling.jake@gmail.com>
2022-03-10 11:29:25 -0500

    Add build configuration

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((projectile-project-configure-cmd . "cmake -DCMAKE_TOOLCHAIN_FILE=x86_64-elf-gcc.cmake -Bbuild")
+         (projectile-project-compilation-cmd . "cmake --build build")
+         (projectile-project-run-cmd . "cmake --build build --target qemu"))))
